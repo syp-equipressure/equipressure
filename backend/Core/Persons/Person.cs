6f4e4a9dc8a083ebb256c05885161e42b8e834a6
orange-panther <katharinaeinzi@gmail.com>
2026-01-10 14:25:39 +0100

    feat: add class Person, CustomerRelation and Role #EP-37 In Progress

--- backend/Core/Persons/Person.cs
+++ backend/Core/Persons/Person.cs
@@ -2,5 +2,42 @@
 
 public class Person
 {
+    public int PersonId { get; set; }
+
+    public required string Firstname { get; set; }
+    public required string Lastname { get; set; }
+    public required string Email { get; set; }
+
+    public int HeightCm { get; set; }
+    public int WeightKg { get; set; }
+
+    public int Plz { get; set; }
+    public required string City { get; set; }
+    public required string Street { get; set; }
+    public int Housenumber { get; set; }
+
+    public string? WebsiteLink { get; set; }
+    public string? Description { get; set; }
     
+    public List<CustomerRelationship> CustomerRelationships { get; set; } = [];
+    public List<Role> Roles { get; set; } = [];
+}
+
+// Assoziationstabelle 
+public class CustomerRelationship
+{
+    public int PersonId { get; set; }
+    public int RelatedPersonId { get; set; }
+
+    // erscheint in der Favouriten-Liste unserer Person
+    public bool IsFavorite { get; set; }
+
+    // erscheint in der Kontakt-Liste unserer Person
+    public bool IsContact { get; set; }
+}
+
+public class Role
+{
+    public int RoleId { get; set; }
+    public required string Name { get; set; }
 }
