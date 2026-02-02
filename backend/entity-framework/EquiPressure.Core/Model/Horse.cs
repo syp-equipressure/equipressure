ffc325e5cbe96bf9ab915996127a8c39db497869
floradallinger <flora.dallinger@gmx.at>
2026-02-02 14:49:16 +0100

    feat: model class horse

--- backend/entity-framework/EquiPressure.Core/Model/Horse.cs
+++ backend/entity-framework/EquiPressure.Core/Model/Horse.cs
@@ -1,8 +1,19 @@
-﻿namespace EquiPressure.Core.Model;
+﻿using NodaTime;
+
+namespace EquiPressure.Core.Model;
 
 public class Horse
 {
-    
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public LocalDate DateOfBirth { get; set; }
+    public decimal Weight { get; set; }
+    public decimal Height { get; set; }
+    public required string Gender { get; set; }
+    public int HorseBreedId { get; set; }
+    public List<HorseBreed> HorseBreeds { get; set; } = null!;
+    public int AddressId { get; set; }
+    public List<Address> HorseAddress { get; set; } = null!;
 }
 
 public class HorseBreed
