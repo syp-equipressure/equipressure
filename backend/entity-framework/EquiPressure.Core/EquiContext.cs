0fe83b34b44a5b0f0119a770e77cc7d57c444158
floradallinger <flora.dallinger@gmx.at>
2026-02-02 15:38:21 +0100

    feat: added the EquiContext and started with the first db sets #EP-49 In Progress

--- backend/entity-framework/EquiPressure.Core/EquiContext.cs
+++ backend/entity-framework/EquiPressure.Core/EquiContext.cs
@@ -1,3 +1,19 @@
-﻿namespace EquiPressure.Core;
+﻿using EquiPressure.Core.Model;
+using Microsoft.EntityFrameworkCore;
 
-public class EquiContext { }
+namespace EquiPressure.Core;
+
+public class EquiContext
+{
+    // Person db sets
+    public DbSet<Person> Person { get; set; }
+    public DbSet<PersonRelationship> PersonRelationships { get; set; }
+    public DbSet<PersonRole> PersonRoles { get; set; }
+    public DbSet<PersonRoleAssignment> PersonRoleAssignments { get; set; }
+    public DbSet<PersonHorse> PersonHorses { get; set; }
+    //Horse db sets
+    public DbSet<Horse> Horses { get; set; }
+    public DbSet<HorseBreed> HorseBreeds { get; set; }
+    public DbSet<Breed> Breeds { get; set; }
+    
+}
