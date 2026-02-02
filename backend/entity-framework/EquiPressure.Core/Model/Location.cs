6e9181812d65fa78e99bb368fb41c42836f2137f
floradallinger <flora.dallinger@gmx.at>
2026-02-02 14:57:17 +0100

    feat: added the model class address

--- backend/entity-framework/EquiPressure.Core/Model/Location.cs
+++ backend/entity-framework/EquiPressure.Core/Model/Location.cs
@@ -2,7 +2,12 @@
 
 public class Address
 {
+    public int Id { get; set; }
+    public required string Street { get; set; }
+    public int HouseNumber { get; set; }
     
+    public int CityId { get; set; }
+    public City City { get; set; } = null!;
 }
 
 public class City
