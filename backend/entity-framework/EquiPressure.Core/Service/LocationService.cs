37c8bfd910c60178da091a8a4a34fe63a67f1b67
floradallinger <flora.dallinger@gmx.at>
2026-02-09 11:09:29 +0100

    feat: last changes

--- backend/entity-framework/EquiPressure.Core/Service/LocationService.cs
+++ backend/entity-framework/EquiPressure.Core/Service/LocationService.cs
@@ -43,7 +43,6 @@ public class LocationService(EquiContext context) : ILocationService
     
     public async ValueTask<GetCityAsyncResult> GetCityAsync(int length, string nameFilter)
     {
-        
         throw new NotImplementedException();
     }
 
