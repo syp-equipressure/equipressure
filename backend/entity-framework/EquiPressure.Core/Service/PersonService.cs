07ec543918c5a95815359dd17d33dfe859074a96
floradallinger <flora.dallinger@gmx.at>
2026-02-09 18:00:12 +0100

    feat: added the two classes to get the name by id and to get all favourites

--- backend/entity-framework/EquiPressure.Core/Service/PersonService.cs
+++ backend/entity-framework/EquiPressure.Core/Service/PersonService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices.ComTypes;
+using Person = EquiPressure.Core.Model.Person;
 using Saddle = EquiPressure.Core.Model.Saddle;
 
 namespace EquiPressure.Core.Service;
@@ -16,17 +17,23 @@ using OneOf.Types;
 using EquiPressure.Core.Model;
 
 using GetPersonAsEquestrianByIdAsyncResult
-    = OneOf.OneOf<OneOf.Types.Success<EquestrianMinimaldata>, OneOf.Types.NotFound>;
+    = OneOf.OneOf<OneOf.Types.Success<EquestrianMinimalData>, OneOf.Types.NotFound>;
 using GetPersonAddressAsyncResult
     = OneOf.OneOf<OneOf.Types.Success<Model.Address>, OneOf.Types.NotFound>;
 using GetPersonAsSaddlerByIdAsyncResult
-    = OneOf.OneOf<OneOf.Types.Success<SaddlerMinimaldata>, IBaseService.InvalidData, OneOf.Types.NotFound>;
+    = OneOf.OneOf<OneOf.Types.Success<SaddlerMinimalData>, IBaseService.InvalidData, OneOf.Types.NotFound>;
+using GetNameOfPersonByIdAsyncResult 
+    = OneOf.OneOf<OneOf.Types.Success<NameData>, OneOf.Types.NotFound>;
+using GetFavouritesFromPersonAsyncResult 
+    = OneOf.OneOf<OneOf.Types.Success<List<Person>>, OneOf.Types.NotFound>;
 
 public interface IPersonService
 {
     public ValueTask<GetPersonAsEquestrianByIdAsyncResult> GetPersonAsEquestrianByIdAsync(int id);
     public ValueTask<GetPersonAddressAsyncResult> GetPersonAddressAsync(int id);
     public ValueTask<GetPersonAsSaddlerByIdAsyncResult> GetPersonAsSaddlerByIdAsync(int saddlerId, int equestrianId);
+    public ValueTask<GetNameOfPersonByIdAsyncResult> GetNameOfPersonByIdAsync(int id);
+    public ValueTask<GetFavouritesFromPersonAsyncResult> GetFavouritesFromPersonAsync(int id);
 }
 
 public class PersonService(EquiContext context) : IPersonService
@@ -40,7 +47,7 @@ public class PersonService(EquiContext context) : IPersonService
                                   .Include(pra => pra.Role)
                                   .Where(pra => pra.Role.Name.ToLower() == "equestrian")
                                   .Where(pra => pra.PersonId == id)
-                                  .Select(pra => new EquestrianMinimaldata
+                                  .Select(pra => new EquestrianMinimalData
                                   (
                                    pra.Person.FirstName,
                                    pra.Person.LastName,
@@ -56,7 +63,7 @@ public class PersonService(EquiContext context) : IPersonService
                                   .FirstOrDefaultAsync();
 
         return result != null 
-            ? new Success<EquestrianMinimaldata>(result) 
+            ? new Success<EquestrianMinimalData>(result) 
             : new NotFound();
     }
 
@@ -95,7 +102,7 @@ public class PersonService(EquiContext context) : IPersonService
                                                                                    (r.Person1Id == saddlerId
                                                                          && r.Person2Id == equestrianId) 
                                     || (r.Person1Id == equestrianId && r.Person2Id == saddlerId)))})
-                            .Select(p => new SaddlerMinimaldata
+                            .Select(p => new SaddlerMinimalData
                             (
                                 p.Person.FirstName,
                                 p.Person.LastName,
@@ -110,13 +117,42 @@ public class PersonService(EquiContext context) : IPersonService
                             .FirstOrDefaultAsync();
         
         return result != null
-            ? new Success<SaddlerMinimaldata>(result)
+            ? new Success<SaddlerMinimalData>(result)
                 : new NotFound();
+    }
+
+    public async ValueTask<GetNameOfPersonByIdAsyncResult> GetNameOfPersonByIdAsync(int id)
+    {
+        var result = await context.Person
+                            .Where(p => p.Id == id)
+                            .Select(p => new NameData(p.FirstName, p.LastName))
+                            .FirstOrDefaultAsync();
+
+        return result != null
+            ? new Success<NameData>(result)
+            : new NotFound();
+    }
+
+    public async ValueTask<GetFavouritesFromPersonAsyncResult> GetFavouritesFromPersonAsync(int id)
+    {
+        var personExists = await context.Person.AnyAsync(p => p.Id == id);
+        if (!personExists)
+        {
+            return new NotFound();
+        }
+        var result = await context.Person
+                            .Include(p => p.Relationships)
+                            .Where(p => p.Id == id)
+                            .Where(p => p.Relationships.All(r => r.IsFavourite))
+                            .ToListAsync();
+        return new Success<List<Person>>(result);
     }
 }
 
-public record EquestrianMinimaldata(string FirstName, string LastName, string? Street, int? HouseNumber, string City,
+public record EquestrianMinimalData(string FirstName, string LastName, string? Street, int? HouseNumber, string City,
                                     string PLZ, string Email, decimal Height, decimal Weight);
                                     
-public record SaddlerMinimaldata(string FirstName, string LastName, string? Street, int? HouseNumber, string City,
+public record SaddlerMinimalData(string FirstName, string LastName, string? Street, int? HouseNumber, string City,
                                  string PLZ, string? Link, string? Description, bool isFavourite);
+                                 
+public record NameData(string FirstName, string LastName);
