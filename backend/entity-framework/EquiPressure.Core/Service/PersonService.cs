9396088a19ab10cb3b45419f645406b8db96f737
floradallinger <flora.dallinger@gmx.at>
2026-02-09 13:40:33 +0100

    struct: added the structure of the person service class

--- backend/entity-framework/EquiPressure.Core/Service/PersonService.cs
+++ backend/entity-framework/EquiPressure.Core/Service/PersonService.cs
@@ -1,6 +1,32 @@
 ﻿namespace EquiPressure.Core.Service;
 
-public class PersonService
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using NodaTime.TimeZones;
+using OneOf;
+using OneOf.Types;
+using EquiPressure.Core.Model;
+
+using GetPersonByIdAsyncResult
+    = OneOf.OneOf<OneOf.Types.Success<Model.City[]>, OneOf.Types.Error>;
+using GetPersonAddressAsyncResult
+    = OneOf.OneOf<OneOf.Types.Success<Model.Address>,
+        ILocationService.InvalidData, OneOf.Types.Error>;
+
+public interface IPersonService
 {
-    
+    public ValueTask<GetPersonByIdAsyncResult> GetPersonByIdAsync(int id);
+    public ValueTask<GetPersonAddressAsyncResult> GetPersonAddressAsync(int id);
+}
+
+public class PersonService : IPersonService
+{
+    public ValueTask<GetPersonByIdAsyncResult> GetPersonByIdAsync(int id) => throw new NotImplementedException();
+
+    public ValueTask<GetPersonAddressAsyncResult> GetPersonAddressAsync(int id) => throw new NotImplementedException();
 }
