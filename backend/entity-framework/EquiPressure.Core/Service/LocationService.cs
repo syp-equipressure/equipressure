501fbdd31c2b28dd2183175e2a76e8e67cac2508
floradallinger <flora.dallinger@gmx.at>
2026-02-09 12:51:08 +0100

    feat: added the two required linq queries for location

--- backend/entity-framework/EquiPressure.Core/Service/LocationService.cs
+++ backend/entity-framework/EquiPressure.Core/Service/LocationService.cs
@@ -12,15 +12,15 @@ using OneOf.Types;
 using EquiPressure.Core.Model;
 
 using GetCityAsyncResult
-    = OneOf.OneOf<OneOf.Types.Success<Model.City>, OneOf.Types.Error>;
+    = OneOf.OneOf<OneOf.Types.Success<Model.City[]>, OneOf.Types.Error>;
 using AddAddressAsyncResult
     = OneOf.OneOf<OneOf.Types.Success<Model.Address>,
         ILocationService.InvalidData, OneOf.Types.Error>;
 
 public interface ILocationService
 {
-    public ValueTask<GetCityAsyncResult> GetCityAsync(int length, string nameFilter);
-    public ValueTask<AddAddressAsyncResult> AddAddressAsync(string street, int houseNumber
+    public ValueTask<GetCityAsyncResult> GetCityAsync(int? length, string? nameFilter);
+    public ValueTask<AddAddressAsyncResult> AddAddressAsync(string? street, int? houseNumber
                                                             , string cityName, string plz);
     public readonly record struct InvalidData;
     
@@ -40,13 +40,78 @@ public class LocationService(EquiContext context) : ILocationService
 
     private Task RollbackAsync() => context.Database.RollbackTransactionAsync();
     
-    public async ValueTask<GetCityAsyncResult> GetCityAsync(int length, string nameFilter)
+    public async ValueTask<GetCityAsyncResult> GetCityAsync(int? length, string? nameFilter)
     {
-        throw new NotImplementedException();
+        var result = context.Addresses
+                            .GroupBy(a => a.City)
+                            .Select(g => new
+                            {
+                                city = g.Key,
+                                count = g.Sum(a => a.Persons.Count)
+                            });
+        if (nameFilter != null)
+        {
+            result = result.Where(g => g.city.Name.ToLower()
+                               .Contains(nameFilter.ToLower()));
+            
+        }
+
+        result = result.OrderBy(r => r.count);
+        
+        if (length != null)
+        {
+            result = result.Take(length.Value);
+        }
+
+        var res = await result.Select(r => r.city).ToArrayAsync();
+
+        return res.Any() ? new Success<City[]>(res) 
+            : new Error();
     }
 
-    public async ValueTask<AddAddressAsyncResult> AddAddressAsync(string street, int houseNumber, string cityName, string plz)
+    public async ValueTask<AddAddressAsyncResult> AddAddressAsync(string? street, int? houseNumber, 
+                                                                  string cityName, string plz)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(cityName) || string.IsNullOrEmpty(plz))
+        {
+            return new ILocationService.InvalidData();
+        }
+
+        await BeginTransactionAsync();
+        
+        try
+        {
+            var city = await context.Cities.FirstOrDefaultAsync(c => c.Name == cityName && c.PLZ == plz) 
+                       ?? new City
+                       {
+                           Name = cityName,
+                           PLZ = plz
+                       };
+            
+            await context.AddAsync(city);
+            await context.SaveChangesAsync();
+
+            var address = await context.Addresses.FirstOrDefaultAsync(a => a.Street == street
+                                                                           && a.HouseNumber == houseNumber
+                                                                           && a.CityId == city.Id)
+                          ?? new Address
+                          {
+                              CityId = city.Id,
+                              City = city,
+                              Street = street,
+                              HouseNumber = houseNumber
+                          };
+            
+            city.Addresses.Add(address);
+            await context.AddAsync(address);
+            await CommitAsync();
+            
+            return new Success<Address>(address);
+        }
+        catch
+        {
+            await RollbackAsync();
+            return new Error();
+        }
     }
 }
