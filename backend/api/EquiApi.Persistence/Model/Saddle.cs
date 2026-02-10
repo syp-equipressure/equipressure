36984d0c8c4fb856343ca0386511592a4afb2e79
orange-panther <katharinaeinzi@gmail.com>
2026-02-10 16:50:48 +0100

    fix: remove counter from saddle in model

--- backend/api/EquiApi.Persistence/Model/Saddle.cs
+++ backend/api/EquiApi.Persistence/Model/Saddle.cs
@@ -18,8 +18,6 @@ public class SaddleCategory
 {
     public int Id { get; set; }
     public required string Name { get; set; }
-    // TODO: Ask Flora what this is supposed to do; you can always just select the count via sql
-    public int Counter { get; set; }
     
     public List<Saddle> Saddles { get; set; } = [];
 }
