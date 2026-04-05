47d3246194a688642cab1a2a8eaec848a74fb73f
floradallinger <flora.dallinger@gmx.at>
2026-04-05 14:09:33 +0200

    fix: forgot to return the correct type

--- backend/api/EquiApi.Persistence/Repositories/DeviceRepository.cs
+++ backend/api/EquiApi.Persistence/Repositories/DeviceRepository.cs
@@ -6,6 +6,8 @@ namespace EquiApi.Persistence.Repositories;
 public interface IDeviceRepository
 {
     public ValueTask<IReadOnlyCollection<MeasurementDevice>> GetDevicesFromUserIdAsync(int userId);
+    public ValueTask<Person> GetOwnerOfDeviceAsync(int deviceId);
+    public ValueTask<IReadOnlyCollection<Person>> GetPersonOfDeviceAsync(int deviceId);
 }
 
 public class DeviceRepository(DbSet<MeasurementDevice> devices) : IDeviceRepository
@@ -16,4 +18,8 @@ public class DeviceRepository(DbSet<MeasurementDevice> devices) : IDeviceReposit
         return await devices.Include(d => d.Users)
                          .Where(d => d.Users.Any(u => u.UserId == userId)).ToListAsync();
     }
+
+    public ValueTask<Person> GetOwnerOfDeviceAsync(int deviceId) => throw new NotImplementedException();
+
+    public ValueTask<IReadOnlyCollection<Person>> GetPersonOfDeviceAsync(int deviceId) => throw new NotImplementedException();
 }
--- backend/api/EquiApi/Controllers/DeviceController.cs
+++ backend/api/EquiApi/Controllers/DeviceController.cs
@@ -23,15 +23,20 @@ public sealed class DeviceController(IDeviceService deviceService,
         
         var result = await deviceService.GetDevicesFromUserIdAsync(userId);
         
-        return result.Match<ActionResult<DeviceListResponse>>(success => Ok(success.Value),
+        return result.Match<ActionResult<DeviceListResponse>>(success => Ok(DeviceListResponse
+                                                                                .FromDeviceList(success.Value)),
                                                               notFound => NotFound());
     }
 }
 
-public sealed record DeviceListResponse(IEnumerable<DeviceDto> Devices);
+public sealed record DeviceListResponse(IEnumerable<DeviceDto> Devices)
+{
+    public static DeviceListResponse FromDeviceList(IReadOnlyCollection<MeasurementDevice> devices) =>
+        new(devices.Select(d => DeviceDto.FromDevice(d)));
+}
 
 public sealed record DeviceDto(int Id, int CategoryId, Person Owner, List<DeviceUser> DeviceUser)
 {
     public static DeviceDto FromDevice(MeasurementDevice device) =>
         new(device.Id, device.CategoryId, device.Owner, device.Users);
-};
+}
