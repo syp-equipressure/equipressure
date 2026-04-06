19373c9d1ecf3b80fbdeb12d701cfdcc0ae0221e
floradallinger <flora.dallinger@gmx.at>
2026-04-06 19:42:14 +0200

    feat: added the last endpoint to the controller... still need to test everything #EP-73 To Verify

--- backend/api/EquiApi/Controllers/DeviceController.cs
+++ backend/api/EquiApi/Controllers/DeviceController.cs
@@ -55,7 +55,29 @@ public sealed class DeviceController(IDeviceService deviceService,
                                                      });
     }
 
-    
+    [HttpGet("{deviceId:int}/users")]
+    [ProducesResponseType<PersonListResponse>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+    public async ValueTask<ActionResult<PersonListResponse>> GetUsersByDeviceId([FromRoute] int deviceId)
+    {
+        if (deviceId < 0)
+        {
+            logger.LogWarning("deviceId {deviceId} has to be a valid number", deviceId);
+            return BadRequest();
+        }
+
+        var result = await deviceService.GetUsersOfDevice(deviceId);
+
+        return result.Match<ActionResult<PersonListResponse>>(
+                                                              success => Ok(PersonListResponse.FromPersonList(success.Value)),
+                                                              notFound => NotFound(),
+                                                              noUsers =>
+                                                              {
+                                                                  logger.LogWarning("Device {deviceId} has no assigned users", noUsers.deviceId);
+                                                                  return UnprocessableEntity();
+                                                              });
+    }
     
 }
 
