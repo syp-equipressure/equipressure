d52407dd814530c67f397b623e08cf534c502fdb
floradallinger <flora.dallinger@gmx.at>
2026-05-03 15:48:33 +0200

    refactor: added the documentation of the device repository

--- backend/api/EquiApi.Persistence/Repositories/DeviceRepository.cs
+++ backend/api/EquiApi.Persistence/Repositories/DeviceRepository.cs
@@ -7,11 +7,36 @@ namespace EquiApi.Persistence.Repositories;
 
 public interface IDeviceRepository
 {
+    /// <summary>
+    /// Checks if the device exists
+    /// </summary>
+    /// <param name="deviceId">id of the device</param>
+    /// <returns>true if it exists false if not</returns>
     public ValueTask<bool> DeviceExistsAsync(int deviceId);
     
+    /// <summary>
+    /// Gets a device by its id
+    /// </summary>
+    /// <param name="deviceId">the id of the device</param>
+    /// <returns>The measurementDevice if it exists</returns>
     public ValueTask<MeasurementDevice?> GetDeviceByIdAsync(int deviceId);
+    /// <summary>
+    /// Gets all devices of a user
+    /// </summary>
+    /// <param name="userId">the id of the user</param>
+    /// <returns>a list of the devices of the user</returns>
     public ValueTask<IReadOnlyCollection<MeasurementDevice>> GetDevicesFromUserIdAsync(int userId);
+    /// <summary>
+    /// Gets the owner of a device
+    /// </summary>
+    /// <param name="deviceId">the id of the device</param>
+    /// <returns>the person object of the owner or a notFound if there is no Owner</returns>
     public ValueTask<OneOf<Person, NotFound>> GetOwnerOfDeviceAsync(int deviceId);
+    /// <summary>
+    /// Gets all the people which are subscribed on a specific device
+    /// </summary>
+    /// <param name="deviceId">the id of the device</param>
+    /// <returns>the list of person objects or a notfound if there are none</returns>
     public ValueTask<OneOf<IReadOnlyCollection<Person>, NotFound>> GetPersonOfDeviceAsync(int deviceId);
 
 }
