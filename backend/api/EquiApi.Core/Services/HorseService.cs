4ba242b0fbb7827cb8c7a6627a00af5274f12e98
floradallinger <flora.dallinger@gmx.at>
2026-05-03 15:17:37 +0200

    feat: added the horse service and implemented the service for the endpoint to get all horses of a user #EP-87 In Progress

--- backend/api/EquiApi.Core/Services/HorseService.cs
+++ backend/api/EquiApi.Core/Services/HorseService.cs
@@ -1,6 +1,28 @@
-﻿namespace EquiApi.Core.Services;
+﻿using EquiApi.Persistence.Model;
+using EquiApi.Persistence.Util;
+using OneOf;
+using OneOf.Types;
 
-public class HorseService
+
+namespace EquiApi.Core.Services;
+
+public interface IHorseService
 {
-    
+    public ValueTask<OneOf<IReadOnlyCollection<Horse>, NotFound>> GetAllHorsesOfPersonAsync(int personId);
+}
+
+public class HorseService(IUnitOfWork uow, ILogger<HorseService> logger) : IHorseService
+{
+    public async ValueTask<OneOf<IReadOnlyCollection<Horse>, NotFound>> GetAllHorsesOfPersonAsync(int personId)
+    {
+        if (await uow.PersonRepository.PersonExists(personId))
+        {
+            logger.LogInformation("Person with id {id} could not be found", personId);
+            return new NotFound();
+        }
+
+        var res = await uow.HorseRepository.GetAllHorsesOfUserAsync(personId);
+
+        return res.ToArray();
+    }
 }
