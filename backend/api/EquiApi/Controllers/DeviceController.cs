0e3920782d6c03ef72c8e6093160f4307399847c
floradallinger <flora.dallinger@gmx.at>
2026-05-03 15:39:56 +0200

    fix: accedentally used a / to many for a route

--- backend/api/EquiApi/Controllers/DeviceController.cs
+++ backend/api/EquiApi/Controllers/DeviceController.cs
@@ -11,7 +11,7 @@ namespace EquiApi.Controllers;
 public sealed class DeviceController(IDeviceService deviceService, 
                               ILogger<DeviceController> logger) : BaseController
 {
-    [HttpGet("/{userId:int}")]
+    [HttpGet("{userId:int}")]
     [ProducesResponseType<DeviceListResponse>(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
