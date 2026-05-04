3f7121ca0b9732e41c2adae9dfae7097cdfd248b
floradallinger <flora.dallinger@gmx.at>
2026-05-04 19:46:51 +0200

    style: used returned instead of thrown

--- backend/api/EquiApi.Core/Services/DeviceService.cs
+++ backend/api/EquiApi.Core/Services/DeviceService.cs
@@ -73,29 +73,29 @@ public interface IDeviceService
         RemoveUserFromDevice(int userId, string deviceId);
 
     /// <summary>
-    /// Thrown when no Owner was found -> repo sent back null
+    /// returned when no Owner was found -> repo sent back null
     /// </summary>
     /// <param name="DeviceId"></param>
     public record NoOwnerFound(string DeviceId);
     
     /// <summary>
-    /// thrown when the list of users is < 1
+    /// returned when the list of users is < 1
     /// </summary>
     /// <param name="DeviceId"></param>
     public record NoUsersFound(string DeviceId);
     
     /// <summary>
-    /// thrown if there are too little users in order to delete one. There has to be at least one user incl owner
+    /// returned if there are too little users in order to delete one. There has to be at least one user incl owner
     /// </summary>
     public record TooLittleUsers();
     
     /// <summary>
-    /// thrown when the given number of users from the devicecategory is passed when adding one user 
+    /// returned when the given number of users from the devicecategory is passed when adding one user 
     /// </summary>
     public record TooManyUsers();
     
     /// <summary>
-    /// thrown if someone tries to delete the owner of a device
+    /// returned if someone tries to delete the owner of a device
     /// </summary>
     public record OwnerCantBeDeleted();
 }
